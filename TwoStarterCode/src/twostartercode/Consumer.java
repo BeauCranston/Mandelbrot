bcc9607427a2edebc759d4b17eda1cde19dfce96
BeauCranston <beau2tie@gmail.com>
2020-10-24 00:06:06 -0400

    added producer and consumer classes

---
+++ TwoStarterCode/src/twostartercode/Consumer.java
@@ -0,0 +1,8 @@
+package twostartercode;
+
+public class Consumer implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
---
+++ TwoStarterCode/src/twostartercode/Producer.java
@@ -0,0 +1,8 @@
+package twostartercode;
+
+public class Producer implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
